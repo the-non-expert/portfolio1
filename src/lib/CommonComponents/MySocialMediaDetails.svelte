b82e7500c6376c0d42fa71cb8ed312c70fdefd2b
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-08-24 15:41:26 +0530

    social media component

--- src/lib/CommonComponents/MySocialMediaDetails.svelte
+++ src/lib/CommonComponents/MySocialMediaDetails.svelte
@@ -1,6 +1,9 @@
 <script>
   import instagram from "$lib/Images/instagram.svg";
   import whatsapp from "$lib/Images/whatsapp.svg";
+  import gmail from "$lib/Images/gmail.svg";
+  import npmIcon from "$lib/Images/npmIcon.svg";
+  import github from "$lib/Images/github.svg";
 </script>
 
 <div class=" flex gap-3">
@@ -11,10 +14,27 @@
       class="w-10 h-10 object-contain border-2 p-2 rounded-xl"
     /></a
   >
-  <a href="https://wa.me/7978284945" target="_blank">
+  <!-- <a href="https://wa.me/7978284945" target="_blank">
     <img
-      src={whatsapp}
-      alt="whatsapp"
+      src={gmail}
+      alt="gmail"
+      class="w-10 h-10 object-contain border-2 p-2 rounded-xl"
+    /></a
+  > -->
+  <a
+    href="https://www.npmjs.com/settings/ayushjhunjhunwala/packages"
+    target="_blank"
+  >
+    <img
+      src={npmIcon}
+      alt="npmIcon"
+      class="w-10 h-10 object-contain border-2 p-2 rounded-xl"
+    /></a
+  >
+  <a href="https://github.com/the-non-expert" target="_blank">
+    <img
+      src={github}
+      alt="github"
       class="w-10 h-10 object-contain border-2 p-2 rounded-xl"
     /></a
   >
